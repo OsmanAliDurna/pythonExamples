4d457c1c499f8584d32e32856e4ce77c54ff97b8
Osman Ali Durna <o_durna@hotmail.com>
2021-02-14 20:52:09 +0200

    .format examples

---
+++ py_10th.py
@@ -0,0 +1,24 @@
+isim = "Osman Ali"
+print("Benim ismim : {}".format(isim))
+
+soyisim = "Durna"
+
+print("İsim Soyisim : {} {}".format(isim, soyisim))
+
+print("İsim Soyisim : {i} {s}".format(i = isim, s = soyisim))
+
+print("Benim ismim {} ve ben {}".format(isim, "mühendisim."))
+
+deger = 22/7
+
+print(deger)
+
+print("Pi : {d:1.4}".format(d = deger))
+
+print("Pi : {d:10.5}".format(d = deger))
+
+# 1-10 değerleri tüm rakamların ve noktanın toplam kaç karakterlik olması gerektiğini gösterir.
+# 2. değer > 1. değer ise ve nokta var ise sonuçta; 1. değer = 2. değer + 1 (kabul edilir) ama nokta yok ise; 1.değer = 2.değer (kabul edilir)
+# 2. değer < 1. değer ise ve nokta var ise sonuçta; 1. değer - (2. değer + 1) adet boşluk sonuçtan önce basılır. Ama nokta yok ise; 1.değer - 2. değer adet boşluk basılır. 
+
+# 4-5 değeri . dan önce ve sonrasında toplam kaç rakam görünsün istediğinizi gösterir.
